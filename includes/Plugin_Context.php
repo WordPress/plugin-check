64bfd750a762032ca5003775596c249c885a657e
jjgrainger <github@jjgrainger.co.uk>
2022-12-15 17:19:12 +0000

    add Plugin_Context class

---
+++ includes/Plugin_Context.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Class WordPress\Plugin_Check\Plugin_Context
+ *
+ * @package plugin-check
+ */
+
+namespace WordPress\Plugin_Check;
+
+/**
+ * Class representing the context in which the plugin is running.
+ *
+ * @since 0.1.0
+ */
+class Plugin_Context {
+	//
+}
