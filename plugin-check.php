381b9975bfb07b0e26ac816e58b70970dd579248
jjgrainger <github@jjgrainger.co.uk>
2022-12-15 17:22:38 +0000

    setup plugin

--- plugin-check.php
+++ plugin-check.php
@@ -14,3 +14,33 @@
  *
  * @package plugin-check
  */
+
+define( 'WP_PLUGIN_CHECK_VERSION', '0.1.0' );
+define( 'WP_PLUGIN_CHECK_MINIMUM_PHP', '5.6' );
+
+/**
+ * Checks basic requirements and loads the plugin.
+ *
+ * @since 0.1.0
+ */
+function wp_plugin_check_load() {
+	// Check for supported PHP version.
+	if ( version_compare( phpversion(), WP_PLUGIN_CHECK_MINIMUM_PHP, '<' ) ) {
+		return;
+	}
+
+	// Check Composer autoloader exists.
+	if ( ! file_exists( plugin_dir_path( __FILE__ ) . 'vendor/autoload.php' ) ) {
+		return;
+	}
+
+	// Load the Composer autoloader.
+	require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+
+	// Setup the plugin.
+	$class_name = 'WordPress\\Plugin_Check\\Plugin_Main';
+	$instance   = new $class_name( __FILE__ );
+	$instance->add_hooks();
+}
+
+wp_plugin_check_load();
