eb75be871e1407de153f940a6da7ce6f9966a97e
jjgrainger <github@jjgrainger.co.uk>
2022-12-19 11:47:25 +0000

    update Plugin_Context class

--- includes/Plugin_Context.php
+++ includes/Plugin_Context.php
@@ -13,5 +13,58 @@ namespace WordPress\Plugin_Check;
  * @since 0.1.0
  */
 class Plugin_Context {
-	// @TODO: To complete as part of issue #5
+
+	/**
+	 * Absolute path of the plugin main file.
+	 *
+	 * @since 0.1.0
+	 * @var string
+	 */
+	protected $main_file;
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $main_file The absolute path to the plugin main file.
+	 */
+	public function __construct( $main_file ) {
+		$this->main_file = $main_file;
+	}
+
+	/**
+	 * Returns the plugin basename.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return string Plugin basename.
+	 */
+	public function basename() {
+		return plugin_basename( $this->main_file );
+	}
+
+	/**
+	 * Returns the absolute path for a relative path to the plugin directory.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $relative_path Optional. Relative path. Default '/'.
+	 * @return string Absolute path.
+	 */
+	public function path( $relative_path = '/' ) {
+		return plugin_dir_path( $this->main_file ) . ltrim( $relative_path . '/' );
+	}
+
+	/**
+	 * Returns the full URL for a pth relative to the plugin directory.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $relative_path Optional. Relative path. Default '/'.
+	 * @return string Full URL.
+	 */
+	public function url( $relative_path = '/' ) {
+		return plugin_dir_url( $this->main_file ) . \ltrim( $relative_path . '/' );
+	}
 }
