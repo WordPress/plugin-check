985bcb33b9b8d522966f9691a4883e1149cf9b4e
jjgrainger <github@jjgrainger.co.uk>
2022-12-22 11:23:49 +0000

    update tests to use more reliable context

--- tests/Plugin_Context_Tests.php
+++ tests/Plugin_Context_Tests.php
@@ -11,26 +11,27 @@ class Plugin_Context_Tests extends WP_UnitTestCase {
 	public function set_up() {
 		parent::set_up();
 
-		$this->plugin_context = new Plugin_Context( 'plugin-check/plugin-check.php' );
+		$this->plugin_name    = basename( dirname( __DIR__ ) );
+		$this->plugin_context = new Plugin_Context( $this->plugin_name . '/plugin-check.php' );
 	}
 
 	public function test_basename() {
-		$this->assertSame( 'plugin-check/plugin-check.php', $this->plugin_context->basename() );
+		$this->assertSame( $this->plugin_name . '/plugin-check.php', $this->plugin_context->basename() );
 	}
 
 	public function test_path() {
-		$this->assertSame( 'plugin-check/', $this->plugin_context->path() );
+		$this->assertSame( $this->plugin_name . '/', $this->plugin_context->path() );
 	}
 
 	public function test_path_with_parameter() {
-		$this->assertSame( 'plugin-check/another/folder', $this->plugin_context->path( '/another/folder' ) );
+		$this->assertSame( $this->plugin_name . '/another/folder', $this->plugin_context->path( '/another/folder' ) );
 	}
 
 	public function test_url() {
-		$this->assertSame( site_url( '/wp-content/plugins/plugin-check/' ), $this->plugin_context->url() );
+		$this->assertSame( WP_PLUGIN_URL . '/' . $this->plugin_name . '/', $this->plugin_context->url() );
 	}
 
 	public function test_url_with_parameter() {
-		$this->assertSame( site_url( '/wp-content/plugins/plugin-check/folder/file.css' ), $this->plugin_context->url( '/folder/file.css' ) );
+		$this->assertSame( WP_PLUGIN_URL . '/' . $this->plugin_name . '/folder/file.css', $this->plugin_context->url( '/folder/file.css' ) );
 	}
 }
--- tests/Plugin_Main_Tests.php
+++ tests/Plugin_Main_Tests.php
@@ -12,12 +12,10 @@ class Plugin_Main_Tests extends WP_UnitTestCase {
 	public function set_up() {
 		parent::set_up();
 
-		$this->plugin_main = new Plugin_Main( 'plugin-check/plugin-check.php' );
+		$this->plugin_main = new Plugin_Main( basename( dirname( __DIR__ ) ) . '/plugin-check.php' );
 	}
 
 	public function test_context() {
-		$context = $this->plugin_main->context();
-
-		$this->assertInstanceOf( Plugin_Context::class, $context );
+		$this->assertInstanceOf( Plugin_Context::class, $this->plugin_main->context() );
 	}
 }
