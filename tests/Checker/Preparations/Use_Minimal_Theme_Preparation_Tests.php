b4a02721aa8b736f48d5ffd0630f3f531da60412
jjgrainger <github@jjgrainger.co.uk>
2023-01-09 11:03:10 +0000

    add tests

---
+++ tests/Checker/Preparations/Use_Minimal_Theme_Preparation_Tests.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Tests for the Check_Context class.
+ *
+ * @package plugin-check
+ */
+
+use WordPress\Plugin_Check\Checker\Preparation;
+use WordPress\Plugin_Check\Checker\Preparations\Use_Minimal_Theme_Preparation;
+
+class Use_Minimal_Theme_Preparation_Tests extends WP_UnitTestCase {
+	public function set_up() {
+		parent::set_up();
+
+		$this->theme_slug = 'wp-empty-theme';
+		$this->theme_dir  = WP_PLUGIN_DIR . '/' . basename( TESTS_PLUGIN_DIR ) . '/test-content/themes';
+
+		$this->preparation = new Use_Minimal_Theme_Preparation( $this->theme_slug, $this->theme_dir );
+
+		$this->cleanup = $this->preparation->prepare();
+	}
+
+	public function tear_down() {
+		parent::tear_down();
+
+		// Run the cleanup function.
+		($this->cleanup)();
+	}
+
+	public function test_implements_preparation_interface() {
+		$this->assertInstanceOf( Preparation::class, $this->preparation );
+	}
+
+	public function test_get_theme_slug() {
+		$this->assertSame( $this->theme_slug, $this->preparation->get_theme_slug() );
+	}
+
+	public function test_get_theme_name() {
+		$this->assertSame( 'WP Empty Theme', $this->preparation->get_theme_name() );
+	}
+
+	public function test_get_theme_root() {
+		$this->assertSame( $this->theme_dir, $this->preparation->get_theme_root() );
+	}
+}
