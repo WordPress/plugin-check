5ff6ee777219562415b6b060d5971334549dd1b5
vishalkakadiya <vishal.kakadiya@rtcamp.com>
2023-01-26 15:57:44 +0530

    Add Runtime_Check and Static_Check interfaces

---
+++ includes/Checker/Runtime_Check.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Interface WordPress\Plugin_Check\Checker\Runtime_Check
+ *
+ * @package plugin-check
+ */
+
+namespace WordPress\Plugin_Check\Checker;
+
+/**
+ * Interface for Runtime checks.
+ *
+ * @since n.e.x.t
+ */
+interface Runtime_Check extends Check {
+
+}
