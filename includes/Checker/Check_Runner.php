3a409dd1f6ab52293f06d1a1c131a53f77112c11
vishalkakadiya <vishal.kakadiya@rtcamp.com>
2023-02-01 19:26:58 +0530

    Add Check_Runner interface

---
+++ includes/Checker/Check_Runner.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Interface WordPress\Plugin_Check\Checker\Check_Runner
+ *
+ * @package plugin-check
+ */
+
+namespace WordPress\Plugin_Check\Checker;
+
+/**
+ * Interface for runner classes like AJAX runner or CLI runner.
+ *
+ * @since n.e.x.t
+ */
+interface Check_Runner {
+
+	/**
+	 * Determine if the current request is intended for the plugin checker.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @return boolean Returns true if the check is for plugin else false.
+	 */
+	public function is_plugin_check();
+
+	/**
+	 * Handles running of the universal preparations depending on which checks requested.
+	 */
+	public function prepare();
+
+	/**
+	 * Run the requested checks against the plugin context and return the results.
+	 *
+	 * @return Check_Result Returns the check results.
+	 */
+	public function run();
+
+}
