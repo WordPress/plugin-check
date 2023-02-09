a5e9f6ebef14fa5cbf645bd6b9aaab224acea98b
jjgrainger <github@jjgrainger.co.uk>
2023-02-09 18:24:00 +0000

    replace properties with methods

--- includes/Checker/Abstract_Check_Runner.php
+++ includes/Checker/Abstract_Check_Runner.php
@@ -19,44 +19,40 @@ use WordPress\Plugin_Check\Checker\Preparations\Universal_Runtime_Preparation;
 abstract class Abstract_Check_Runner implements Check_Runner {
 
 	/**
-	 * Instance of the Checks class.
-	 *
-	 * @since n.e.x.t
-	 * @var Checks
-	 */
-	protected $checks;
-
-	/**
-	 * Instance of the Check_Context class.
-	 *
-	 * @since n.e.x.t
-	 * @var Check_Context
-	 */
-	protected $context;
-
-	/**
-	 * Array of Check instances to run.
-	 *
-	 * @since n.e.x.t
-	 * @var array
-	 */
-	protected $checks_to_run;
-
-	/**
 	 * Determines if the current request is intended for the plugin checker.
 	 *
 	 * @since n.e.x.t
 	 *
-	 * @return boolean Returns true if the check is for plugin else false.
+	 * @return bool Returns true if the check is for plugin else false.
 	 */
 	abstract public function is_plugin_check();
 
 	/**
-	 * Sets up the check context, checks and preparations based on the request.
+	 * Creates and returns an instance of the Checks class based on the request.
 	 *
 	 * @since n.e.x.t
+	 *
+	 * @return Checks An instances of the Checks class.
 	 */
-	abstract protected function setup_checks();
+	abstract protected function get_checks();
+
+	/**
+	 * Creates and returns the Check_Context for the plugin to check based on the request.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @return Check_Context The Check_Context for the plugin to check.
+	 */
+	abstract protected function get_context();
+
+	/**
+	 * Creates and returns an array of Check instances to run based on the request.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @return array An array of Check instances.
+	 */
+	abstract protected function get_checks_to_run();
 
 	/**
 	 * Prepares the environment for running the requested checks.
@@ -68,12 +64,12 @@ abstract class Abstract_Check_Runner implements Check_Runner {
 	 * @throws Exception Thrown exception when preparation fails.
 	 */
 	public function prepare() {
-		if ( ! $this->requires_universal_preparations( $this->checks_to_run ) ) {
-			$preparation = new Universal_Runtime_Preparation( $this->context );
+		if ( ! $this->requires_universal_preparations( $this->get_checks_to_run() ) ) {
+			$preparation = new Universal_Runtime_Preparation( $this->get_context() );
 			return $preparation->prepare();
 		}
 
-		return null;
+		return function() {};
 	}
 
 	/**
@@ -84,8 +80,7 @@ abstract class Abstract_Check_Runner implements Check_Runner {
 	 * @return Check_Result
 	 */
 	public function run() {
-		$checks = $this->checks->get_checks( $this->checks_to_run );
-
+		$checks       = $this->get_checks_to_run();
 		$preparations = $this->get_shared_preparations( $checks );
 		$cleanups     = array();
 
@@ -94,7 +89,7 @@ abstract class Abstract_Check_Runner implements Check_Runner {
 			$cleanups[] = $instance->prepare();
 		}
 
-		$results = $this->checks->run_checks( $checks );
+		$results = $this->get_checks()->run_checks( $checks );
 
 		if ( ! empty( $cleanups ) ) {
 			foreach ( $cleanups as $cleanup ) {
@@ -111,8 +106,7 @@ abstract class Abstract_Check_Runner implements Check_Runner {
 	 * @since n.e.x.t
 	 *
 	 * @param array $checks An array of check instances to run.
-	 *
-	 * @return boolean Returns true if one or more checks requires the universal runtime preparation.
+	 * @return bool Returns true if one or more checks requires the universal runtime preparation.
 	 */
 	protected function requires_universal_preparations( array $checks ) {
 		foreach ( $checks as $check ) {
@@ -130,7 +124,6 @@ abstract class Abstract_Check_Runner implements Check_Runner {
 	 * @since n.e.x.t
 	 *
 	 * @param array $checks An array of Check instances to run.
-	 *
 	 * @return array An array of reparations to run.
 	 */
 	private function get_shared_preparations( array $checks ) {
