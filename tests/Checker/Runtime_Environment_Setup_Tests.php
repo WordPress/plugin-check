d6e71900d4e0124da37b870c4a0356aae19f287b
jjgrainger <github@jjgrainger.co.uk>
2023-02-21 13:59:31 +0000

    add tests with existing object-cache.php file

--- tests/Checker/Runtime_Environment_Setup_Tests.php
+++ tests/Checker/Runtime_Environment_Setup_Tests.php
@@ -29,10 +29,49 @@ class Runtime_Environment_Setup_Tests extends WP_UnitTestCase {
 
 		$runtime_setup = new Runtime_Environment_Setup();
 		$runtime_setup->setup();
+
+		// Simulate file exists by setting constant found in object-cache.php.
+		define( 'WP_PLUGIN_CHECK_OBJECT_CACHE_DROPIN_VERSION', 1 );
+
 		$runtime_setup->cleanup();
 
 		$this->assertTrue( 0 <= strpos( $wpdb->last_query, 'wppc_' ) );
 		$this->assertFalse( $wp_filesystem->exists( WP_CONTENT_DIR . '/object-cache.php' ) );
+	}
+
+	public function test_setup_with_existing_object_cache() {
+		global $wp_filesystem, $wpdb;
+
+		$this->set_up_mock_filesystem();
+
+		// Simulate a different object-cache.php.
+		$dummy_file_content = '<?php /* Empty object-cache.php drop-in file. */';
+		$wp_filesystem->put_contents( WP_CONTENT_DIR . '/object-cache.php', $dummy_file_content );
+
+		$runtime_setup = new Runtime_Environment_Setup();
+		$runtime_setup->setup();
+
+		$this->assertTrue( 0 <= strpos( $wpdb->last_query, 'wppc_' ) );
+		$this->assertTrue( $wp_filesystem->exists( WP_CONTENT_DIR . '/object-cache.php' ) );
+		$this->assertSame( $dummy_file_content, $wp_filesystem->get_contents( WP_CONTENT_DIR . '/object-cache.php' ) );
+	}
+
+	public function test_cleanup_with_existing_object_cache() {
+		global $wp_filesystem, $wpdb;
+
+		$this->set_up_mock_filesystem();
+
+		// Simulate a different object-cache.php.
+		$dummy_file_content = '<?php /* Empty object-cache.php drop-in file. */';
+		$wp_filesystem->put_contents( WP_CONTENT_DIR . '/object-cache.php', $dummy_file_content );
+
+		$runtime_setup = new Runtime_Environment_Setup();
+		$runtime_setup->setup();
+		$runtime_setup->cleanup();
+
+		$this->assertTrue( 0 <= strpos( $wpdb->last_query, 'wppc_' ) );
+		$this->assertTrue( $wp_filesystem->exists( WP_CONTENT_DIR . '/object-cache.php' ) );
+		$this->assertSame( $dummy_file_content, $wp_filesystem->get_contents( WP_CONTENT_DIR . '/object-cache.php' ) );
 	}
 
 	private function set_up_mock_filesystem() {
