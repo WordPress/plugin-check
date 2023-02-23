1e2ff0635049978311a5b5ca93a125995e12dc3d
jjgrainger <github@jjgrainger.co.uk>
2023-02-23 21:01:17 +0000

    call wp_head() and wp_footer() so scripts are done

--- includes/Checker/Checks/Enqueued_Scripts_Size_Check.php
+++ includes/Checker/Checks/Enqueued_Scripts_Size_Check.php
@@ -170,6 +170,8 @@ class Enqueued_Scripts_Size_Check extends Abstract_Runtime_Check implements With
 		// directly. This is discouraged, but some plugins or themes are still doing it.
 		ob_start();
 		wp_enqueue_scripts();
+		wp_head();
+		wp_footer();
 		ob_get_clean();
 
 		$plugin_scripts     = array();
