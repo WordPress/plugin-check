18f7240999ec7c8f420a4e5edb680ecf56f689db
jjgrainger <github@jjgrainger.co.uk>
2023-02-24 13:55:36 +0000

    update wp_object_cache usage in tests

--- tests/Admin/Admin_Page_Tests.php
+++ tests/Admin/Admin_Page_Tests.php
@@ -52,9 +52,8 @@ class Admin_Page_Tests extends WP_UnitTestCase {
 	public function test_render_page() {
 		global $wp_object_cache;
 
-		// Backup original and create a new WP_Object_Cache.
-		$original_cache  = $wp_object_cache;
-		$wp_object_cache = new WP_Object_Cache();
+		// Backup original plugins in the object cache.
+		$original_plugins = $wp_object_cache->get( 'plugins', 'plugins' );
 
 		// Create the basic information required get_available_plugins.
 		$expected_plugins = array(
@@ -70,10 +69,11 @@ class Admin_Page_Tests extends WP_UnitTestCase {
 		);
 
 		// Include the Plugin Checker plugin.
-		$expected_plugins[ plugin_basename( WP_PLUGIN_CHECK_MAIN_FILE ) ] = array( 'Name' => 'Plugin Checker' );
+		$plugin_basename                      = plugin_basename( WP_PLUGIN_CHECK_MAIN_FILE );
+		$expected_plugins[ $plugin_basename ] = array( 'Name' => 'Plugin Checker' );
 
 		// Set the expected plugins in the cache.
-		$wp_object_cache->add( 'plugins', array( '' => $expected_plugins ), 'plugins' );
+		$wp_object_cache->set( 'plugins', array( '' => $expected_plugins ), 'plugins' );
 
 		// Render the admin page.
 		ob_start();
@@ -82,13 +82,13 @@ class Admin_Page_Tests extends WP_UnitTestCase {
 		ob_end_clean();
 
 		// Restore the original cache.
-		$wp_object_cache = $original_cache;
+		$wp_object_cache->set( 'plugins', $original_plugins, 'plugins' );
 
 		// Remove the plugin checker from exptected plugins for testing.
-		unset( $expected_plugins[ plugin_basename( WP_PLUGIN_CHECK_MAIN_FILE ) ] );
+		unset( $expected_plugins[ $plugin_basename ] );
 
 		// Assert the Plugin Checker does not appear in the select dropdown.
-		$this->assertStringNotContainsString( plugin_basename( WP_PLUGIN_CHECK_MAIN_FILE ), $output );
+		$this->assertStringNotContainsString( $plugin_basename, $output );
 
 		// Assert the expected plugins appear in the select dropdown.
 		foreach ( $expected_plugins as $plugin => $data ) {
@@ -100,9 +100,8 @@ class Admin_Page_Tests extends WP_UnitTestCase {
 	public function test_render_page_with_no_plugins() {
 		global $wp_object_cache;
 
-		// Backup original and create a new WP_Object_Cache.
-		$original_cache  = $wp_object_cache;
-		$wp_object_cache = new WP_Object_Cache();
+		// Backup original plugins in the object cache.
+		$original_plugins = $wp_object_cache->get( 'plugins', 'plugins' );
 
 		// Set the expected plugins to be empty in the cache.
 		$wp_object_cache->add( 'plugins', array( '' => array() ), 'plugins' );
@@ -114,7 +113,7 @@ class Admin_Page_Tests extends WP_UnitTestCase {
 		ob_end_clean();
 
 		// Restore the original cache.
-		$wp_object_cache = $original_cache;
+		$wp_object_cache->set( 'plugins', $original_plugins, 'plugins' );
 
 		$this->assertStringContainsString( 'No plugins available.', $output );
 		$this->assertStringNotContainsString( '<select id="plugin-check__plugins"', $output );
