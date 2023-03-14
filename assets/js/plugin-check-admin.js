536a09ddbe7bddd01cc409ce1bb6586918fb1c0d
jjgrainger <github@jjgrainger.co.uk>
2023-03-14 15:19:22 +0000

    update js to fetch get_checks_to_run first

--- assets/js/plugin-check-admin.js
+++ assets/js/plugin-check-admin.js
@@ -8,15 +8,21 @@
 		return;
 	}
 
-	checkItButton.addEventListener( 'click', (e) => {
+	// Create a state object for the plugin checker.
+	const pluginChecker = {
+		plugin: false,
+		checks_to_run: [],
+	};
+
+	checkItButton.addEventListener( 'click', ( e ) => {
 		e.preventDefault();
 
-		const pluginCheckData = new FormData();
-
 		// Collect the data to pass along for generating a check results.
-		pluginCheckData.append( 'action', 'plugin_check_run_checks' );
+		const pluginCheckData = new FormData();
 		pluginCheckData.append( 'nonce', data.nonce );
 		pluginCheckData.append( 'plugin', pluginsList.value );
+		pluginCheckData.append( 'checks', [] );
+		pluginCheckData.append( 'action', 'plugin_check_get_checks_to_run' );
 
 		fetch(
 			ajaxurl,
@@ -33,33 +39,100 @@
 		)
 		.then(
 			( data ) => {
-				if ( ! data ) {
-					throw new Error( 'Response contains no data' );
+				return handleDataErrors( data );
+			}
+		)
+		.then(
+			( data ) => {
+				console.log( 'plugin_check_get_checks_to_run', data );
+
+				if ( ! data.data || ! data.data.plugin || ! data.data.checks ) {
+					throw new Error( 'Plugin and Checks are missing from the response.' );
 				}
 
-				if ( ! data.success ) {
-					// // If not successful and no message in the response.
-					if ( ! data.data || ! data.data[0].message ) {
-						throw new Error( 'Response contains no data' );
-					}
+				// Store the plugin and checks to run to be used later.
+				pluginChecker.plugin = data.data.plugin;
+				pluginChecker.checks = data.data.checks;
 
-					// If not successful and there is a message in the response.
-					throw new Error( data.data[0].message );
-				}
+				runChecks();
+			}
+		)
+		.catch(
+			( error ) => { console.error( error ) }
+		);
+	} );
 
+
+	/**
+	 * Run Checks.
+	 *
+	 * @since n.e.x.t
+	 */
+	function runChecks() {
+		const pluginCheckData = new FormData();
+		pluginCheckData.append( 'nonce', data.nonce );
+		pluginCheckData.append( 'plugin', pluginChecker.plugin );
+		pluginCheckData.append( 'checks', pluginChecker.checks );
+		pluginCheckData.append( 'action', 'plugin_check_run_checks' );
+
+		fetch(
+			ajaxurl,
+			{
+				method: 'POST',
+				credentials: 'same-origin',
+				body: pluginCheckData
+			}
+		)
+		.then(
+			( response ) => {
+				return response.json();
+			}
+		)
+		.then(
+			( data ) => {
+				return handleDataErrors(data);
+			}
+		)
+		.then(
+			( data ) => {
 				// If the response is successful and there is no message in the response.
 				if ( ! data.data || ! data.data.message ) {
 					throw new Error( 'Response contains no data' );
 				}
 
 				// If the response is successful and there is a message in the response.
-				console.log( data.data.message );
+				console.log( 'plugin_check_run_checks', data.data.message );
 			}
 		)
 		.catch(
 			( error ) => { console.error( error ); }
 		);
+	}
 
-	} );
+	/**
+	 * Handles any errors in the data returned from the response.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param {Object} data The response data.
+	 * @return {Object} The response data.
+	 */
+	function handleDataErrors( data ) {
+		if ( ! data ) {
+			throw new Error( 'Response contains no data' );
+		}
+
+		if ( ! data.success ) {
+			// If not successful and no message in the response.
+			if ( ! data.data || ! data.data[0].message ) {
+				throw new Error( 'Response contains no data' );
+			}
+
+			// If not successful and there is a message in the response.
+			throw new Error( data.data[0].message );
+		}
+
+		return data;
+	}
 
 } )( PLUGIN_CHECK ); /* global PLUGIN_CHECK */
