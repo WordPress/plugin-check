9719698c30f17fc76f044da067bf9ddfd9939b20
jjgrainger <github@jjgrainger.co.uk>
2023-03-21 17:14:50 +0000

    add check for object-cache.php constant

--- includes/Admin/Admin_AJAX.php
+++ includes/Admin/Admin_AJAX.php
@@ -128,7 +128,7 @@ class Admin_AJAX {
 		$message = __( 'Runtime environment was not prepared, cleanup was not run.', 'plugin-check' );
 
 		// Test if the runtime environment tables exist.
-		if ( 'wppc_posts' === $wpdb->get_var( "SHOW TABLES LIKE 'wppc_posts'" ) ) {
+		if ( 'wppc_posts' === $wpdb->get_var( "SHOW TABLES LIKE 'wppc_posts'" ) || defined( 'WP_PLUGIN_CHECK_OBJECT_CACHE_DROPIN_VERSION' ) ) {
 			$runtime = new Runtime_Environment_Setup();
 			$runtime->cleanup();
 			$message = __( 'Runtime environment cleanup successful.', 'plugin-check' );
