bdea4779654632da7cbc71f1ffc369517a3e211a
Jonny Harris <jon@spacedmonkey.co.uk>
2023-03-30 20:16:53 +0100

    Fix lint

--- tests/phpstan/stubs/formatter.php
+++ tests/phpstan/stubs/formatter.php
@@ -4,8 +4,8 @@ namespace WP_CLI;
 
 class Formatter {
 	/**
-	 * @param array $assoc_args Output format arguments.
-	 * @param array $fields Fields to display of each item.
+	 * @param array       $assoc_args Output format arguments.
+	 * @param array       $fields Fields to display of each item.
 	 * @param string|bool $prefix Check if fields have a standard prefix.
 	 * False indicates empty prefix.
 	 */
@@ -15,7 +15,7 @@ class Formatter {
 	 * Display multiple items according to the output arguments.
 	 *
 	 * @param array|Iterator $items The items to display.
-	 * @param bool|array      $ascii_pre_colorized Optional. A boolean or an array of booleans to pass to `format()` if items in the table are pre-colorized. Default false.
+	 * @param bool|array     $ascii_pre_colorized Optional. A boolean or an array of booleans to pass to `format()` if items in the table are pre-colorized. Default false.
 	 */
 	public function display_items( $items, $ascii_pre_colorized = false ) {}
 }
