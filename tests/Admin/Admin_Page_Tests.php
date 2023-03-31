b5e4bbf4cba8be6a74d0b36b4f1c396ec93ed6f5
Jonny Harris <jon@spacedmonkey.co.uk>
2023-03-31 13:22:28 +0100

    Fix lints

--- tests/Admin/Admin_Page_Tests.php
+++ tests/Admin/Admin_Page_Tests.php
@@ -40,7 +40,7 @@ class Admin_Page_Tests extends WP_UnitTestCase {
 		set_current_screen( 'dashboard' );
 
 		$this->admin_page->add_page();
-		$page_hook    = "admin_page_plugin-check";
+		$page_hook    = 'admin_page_plugin-check';
 		$parent_pages = $_parent_pages;
 
 		set_current_screen( $current_screen );
