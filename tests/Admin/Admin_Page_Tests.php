1218f599336c8a708913deb6af9cf24df93f7a38
Jonny Harris <jon@spacedmonkey.co.uk>
2023-04-24 17:50:23 +0100

    Fix lint

--- tests/Admin/Admin_Page_Tests.php
+++ tests/Admin/Admin_Page_Tests.php
@@ -40,7 +40,7 @@ class Admin_Page_Tests extends WP_UnitTestCase {
 		set_current_screen( 'dashboard' );
 
 		$this->admin_page->add_and_initialize_page();
-		$page_hook = $this->admin_page->add_page();
+		$page_hook    = $this->admin_page->add_page();
 		$parent_pages = $_parent_pages;
 
 		set_current_screen( $current_screen );
