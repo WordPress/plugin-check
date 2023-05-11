2f6eafb641b0a4dc7680f6ce735d151bb63922b6
Dion Hulse <dion@wordpress.org>
2023-05-11 12:40:42 +1000

    Add local environment, initial setup for unit tests. (#31)
    * Add wp-env
    * Add unit testing setup
    * Add GitHub actions workflows

---
+++ README.md
@@ -0,0 +1,23 @@
+Plugin Check
+===============
+* Contributors: 
+* Requires at least: 6.2
+* Tested up to: 6.2
+* License: GPLv2 or later
+* License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Plugin Check is a tool for the WordPress.org plugins review team.
+
+## Description #
+
+Plugin Check is a tool for the WordPress.org plugins review team.
+This is intended as a generic tool that is not at all intended on being complete.
+
+Setup steps:
+ - `npm install`
+ - `npm run wp-env start`
+ - `npm run setup:tools`
+
+Commands:
+ - `npm run wp-env start`
+ - `npm test`
---
+++ tests/bootstrap.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped -- This is a shell script.
+ * phpcs:disable WordPress.WP.GlobalVariablesOverride.Prohibited -- This is intentional and necessary.
+ */
+
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+
+if ( ! $_tests_dir ) {
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php";
+	exit( 1 );
+}
+
+require_once dirname( __DIR__ ) . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
+
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+	require dirname( __DIR__ ) . '/plugin.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
