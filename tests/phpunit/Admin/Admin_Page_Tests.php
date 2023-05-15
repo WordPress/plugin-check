83095b871c75bb6696d748cafdf712293964946b
Pascal Birchler <pascalb@google.com>
2023-05-15 13:58:46 +0200

    Lint fix

--- tests/phpunit/Admin/Admin_Page_Tests.php
+++ tests/phpunit/Admin/Admin_Page_Tests.php
@@ -28,7 +28,7 @@ class Admin_Page_Tests extends WP_UnitTestCase {
 	}
 
 	public function test_add_and_initialize_page() {
-		$this->markTestSkipped('Requires investigation due to failure on WordPress 6.0');
+		$this->markTestSkipped( 'Requires investigation due to failure on WordPress 6.0' );
 
 		global $_parent_pages;
 
