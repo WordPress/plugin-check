4f587d769af04cf25bcbd0a8b557e6f926d4efd2
jjgrainger <github@jjgrainger.co.uk>
2023-05-25 10:53:02 +0100

    remove constructor args from check

--- includes/Checker/Abstract_Check_Runner.php
+++ includes/Checker/Abstract_Check_Runner.php
@@ -361,7 +361,7 @@ abstract class Abstract_Check_Runner implements Check_Runner {
 			'wp_plugin_check_checks',
 			array(
 				'i18n_usage'            => new Checks\I18n_Usage_Check(),
-				'enqueued_scripts_size' => new Checks\Enqueued_Scripts_Size_Check( 1 ),
+				'enqueued_scripts_size' => new Checks\Enqueued_Scripts_Size_Check(),
 			)
 		);
 
