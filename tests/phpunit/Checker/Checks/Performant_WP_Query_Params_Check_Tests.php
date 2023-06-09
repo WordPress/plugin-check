52f0f6fcb043efd6610d0daf6c19f96d2d974270
jjgrainger <github@jjgrainger.co.uk>
2023-06-09 16:41:40 +0100

    add tests

---
+++ tests/phpunit/Checker/Checks/Performant_WP_Query_Params_Check_Tests.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Tests for the Performant_WP_Query_Params_Check class.
+ *
+ * @package plugin-check
+ */
+
+use WordPress\Plugin_Check\Checker\Check_Context;
+use WordPress\Plugin_Check\Checker\Check_Result;
+use WordPress\Plugin_Check\Checker\Checks\Performant_WP_Query_Params_Check;
+
+class Performant_WP_Query_Params_Check_Tests extends WP_UnitTestCase {
+
+	public function test_run_with_errors() {
+		$performant_query = new Performant_WP_Query_Params_Check();
+		$check_context    = new Check_Context( TESTS_PLUGIN_DIR . '/tests/phpunit/testdata/plugins/test-plugin-performant-wp-query-params-errors/load.php' );
+		$check_result     = new Check_Result( $check_context );
+
+		$performant_query->run( $check_result );
+
+		$errors = $check_result->get_errors();
+		$warnings = $check_result->get_warnings();
+
+		$this->assertNotEmpty( $errors );
+		$this->assertArrayHasKey( 'load.php', $errors );
+		$this->assertEquals( 2, $check_result->get_error_count() );
+
+		// Check for WordPress.DB.SlowDBQuery error on Line no 22 and column no at 5.
+		$this->assertArrayHasKey( 22, $errors['load.php'] );
+		$this->assertArrayHasKey( 5, $errors['load.php'][22] );
+		$this->assertArrayHasKey( 'code', $errors['load.php'][22][5][0] );
+		$this->assertEquals( 'WordPress.DB.SlowDBQuery', $errors['load.php'][22][5][0]['code'] );
+
+	}
+
+	public function test_run_without_errors() {
+		$performant_query = new Performant_WP_Query_Params_Check();
+		$check_context    = new Check_Context( TESTS_PLUGIN_DIR . '/tests/phpunit/testdata/plugins/test-plugin-performant-wp-query-params-without-errors/load.php' );
+		$check_result     = new Check_Result( $check_context );
+
+		$performant_query->run( $check_result );
+
+		$errors   = $check_result->get_errors();
+		$warnings = $check_result->get_warnings();
+
+		$this->assertEmpty( $errors );
+		$this->assertEmpty( $warnings );
+		$this->assertEquals( 0, $check_result->get_error_count() );
+		$this->assertEquals( 0, $check_result->get_warning_count() );
+	}
+}
---
+++ tests/phpunit/testdata/plugins/test-plugin-performant-wp-query-params-errors/load.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Plugin Name: Test Plugin Performant WP_Query with errors
+ * Plugin URI: https://github.com/wordpress/plugin-check
+ * Description: Plugin Check plugin from the WordPress Performance Team, a collection of tests to help improve plugin performance.
+ * Requires at least: 6.0
+ * Requires PHP: 5.6
+ * Version: n.e.x.t
+ * Author: WordPress Performance Team
+ * Author URI: https://make.wordpress.org/performance/
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: test-plugin-check
+ *
+ * @package test-plugin-check
+ */
+
+/**
+ * File contains errors related to Performant WP_Query params check.
+ */
+
+ $query = new WP_Query(
+	array(
+		'post__not_in'   => array( 1, 2, 3 ),
+		'posts_per_page' => -1,
+		'cache_results'  => false,
+		'meta_key'       => 'age',
+		'meta_query'     => array(
+			array(
+				'key'     => 'age',
+				'value'   => array( 3, 4 ),
+				'compare' => 'IN',
+			),
+		),
+		'tax_query'      => array(
+			array(
+				'taxonomy' => 'custom_taxonomy_slug',
+				'operator' => 'EXISTS',
+			),
+		),
+	)
+);
