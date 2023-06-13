4db0206a2390f160e164c687384ffc507033dd47
Mukesh Panchal <mukeshpanchal27@gmail.com>
2023-06-13 16:34:43 +0530

    Reset PHP_CodeSniffer config

--- includes/Checker/Checks/Abstract_PHP_CodeSniffer_Check.php
+++ includes/Checker/Checks/Abstract_PHP_CodeSniffer_Check.php
@@ -74,6 +74,18 @@ abstract class Abstract_PHP_CodeSniffer_Check implements Static_Check {
 		// Backup the original command line arguments.
 		$orig_cmd_args = $_SERVER['argv'];
 
+		/*
+		 * Reset \PHP_CodeSniffer\Config::$overriddenDefaults to prevent
+		 * incorrect results when running multiple checks.
+		 *
+		 * PHPStan ignore reason: PHPStan raised an issue because we can't
+		 * use class in ReflectionClass.
+		 *
+		 * @phpstan-ignore-next-line
+		 */
+		$reflected_phpcs_config = new \ReflectionClass( '\PHP_CodeSniffer\Config' );
+		$reflected_phpcs_config->setStaticPropertyValue( 'overriddenDefaults', array() );
+
 		// Create the default arguments for PHPCS.
 		$defaults = array(
 			'',
