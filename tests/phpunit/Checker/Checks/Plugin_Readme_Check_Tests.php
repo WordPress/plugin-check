f41a3e2c6d6c79c524c4389d6070522c9df6d4fd
Jonny Harris <jon@spacedmonkey.co.uk>
2023-06-21 12:23:50 +0100

    Allow readme.md

--- tests/phpunit/Checker/Checks/Plugin_Readme_Check_Tests.php
+++ tests/phpunit/Checker/Checks/Plugin_Readme_Check_Tests.php
@@ -112,4 +112,20 @@ class Plugin_Readme_Check_Tests extends WP_UnitTestCase {
 		$this->assertEquals( 0, $check_result->get_error_count() );
 		$this->assertEquals( 0, $check_result->get_warning_count() );
 	}
+
+	public function test_run_md_without_errors() {
+		$readme_check  = new Plugin_Readme_Check();
+		$check_context = new Check_Context( UNIT_TESTS_PLUGIN_DIR . 'test-plugin-plugin-readme-md-without-errors/load.php' );
+		$check_result  = new Check_Result( $check_context );
+
+		$readme_check->run( $check_result );
+
+		$errors   = $check_result->get_errors();
+		$warnings = $check_result->get_warnings();
+
+		$this->assertEmpty( $errors );
+		$this->assertEmpty( $warnings );
+		$this->assertEquals( 0, $check_result->get_error_count() );
+		$this->assertEquals( 0, $check_result->get_warning_count() );
+	}
 }
