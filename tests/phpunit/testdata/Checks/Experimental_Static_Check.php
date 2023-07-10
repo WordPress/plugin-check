34b88feff914393b31fa04290e4c3686330a9c8e
Felix Arntz <felixarntz@google.com>
2023-07-10 11:28:00 -0400

    Add unrelated test failures due to missing import.

--- tests/phpunit/testdata/Checks/Experimental_Static_Check.php
+++ tests/phpunit/testdata/Checks/Experimental_Static_Check.php
@@ -2,6 +2,7 @@
 
 namespace WordPress\Plugin_Check\Test_Data;
 
+use WordPress\Plugin_Check\Checker\Check_Categories;
 use WordPress\Plugin_Check\Checker\Check_Result;
 use WordPress\Plugin_Check\Checker\Static_Check;
 use WordPress\Plugin_Check\Traits\Experimental_Check;
