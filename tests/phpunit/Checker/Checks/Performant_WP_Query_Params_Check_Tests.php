8c6c6be1cf70891edc72d83d5b4a975c3edd5d6b
Mukesh Panchal <mukeshpanchal27@gmail.com>
2023-08-28 15:40:00 +0530

    Bump dependencies

--- tests/phpunit/Checker/Checks/Performant_WP_Query_Params_Check_Tests.php
+++ tests/phpunit/Checker/Checks/Performant_WP_Query_Params_Check_Tests.php
@@ -32,15 +32,15 @@ class Performant_WP_Query_Params_Check_Tests extends WP_UnitTestCase {
 
 		// Check for WordPress.DB.SlowDBQuery.slow_db_query_meta_query warning.
 		$this->assertArrayHasKey( 27, $warnings['load.php'] );
-		$this->assertArrayHasKey( 26, $warnings['load.php'][27] );
-		$this->assertArrayHasKey( 'code', $warnings['load.php'][27][26][0] );
-		$this->assertEquals( 'WordPress.DB.SlowDBQuery.slow_db_query_meta_query', $warnings['load.php'][27][26][0]['code'] );
+		$this->assertArrayHasKey( 9, $warnings['load.php'][27] );
+		$this->assertArrayHasKey( 'code', $warnings['load.php'][27][9][0] );
+		$this->assertEquals( 'WordPress.DB.SlowDBQuery.slow_db_query_meta_query', $warnings['load.php'][27][9][0]['code'] );
 
 		// Check for WordPress.DB.SlowDBQuery.slow_db_query_tax_query warning.
 		$this->assertArrayHasKey( 34, $warnings['load.php'] );
-		$this->assertArrayHasKey( 26, $warnings['load.php'][34] );
-		$this->assertArrayHasKey( 'code', $warnings['load.php'][34][26][0] );
-		$this->assertEquals( 'WordPress.DB.SlowDBQuery.slow_db_query_tax_query', $warnings['load.php'][34][26][0]['code'] );
+		$this->assertArrayHasKey( 9, $warnings['load.php'][34] );
+		$this->assertArrayHasKey( 'code', $warnings['load.php'][34][9][0] );
+		$this->assertEquals( 'WordPress.DB.SlowDBQuery.slow_db_query_tax_query', $warnings['load.php'][34][9][0]['code'] );
 	}
 
 	public function test_run_without_errors() {
