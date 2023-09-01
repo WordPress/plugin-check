4587ef1c268efadfb6bca62470defe45bd24c02c
Gustavo Bordoni <bordoni.dev@gmail.com>
2023-09-01 17:37:40 -0400

    Include changelog and docblock for #45

--- checks/check-constants.php
+++ checks/check-constants.php
@@ -4,19 +4,22 @@ use WordPressdotorg\Plugin_Check\{Error, Guideline_Violation, Message, Notice, W
 
 class Code_Constants extends Check_Base {
 
+	/**
+	 * Checks if any PHP files contain the `ALLOW_UNFILTERED_UPLOADS` constant. Not permitted.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void|Error
+	 */
 	public function check_allow_unfiltered_uploads() {
-
 		if ( ! $this->scan_matching_files_for_needle( 'ALLOW_UNFILTERED_UPLOADS', '\.php$' ) ) {
-
 			return;
-
 		}
 
 		return new Error(
 			'allow_unfiltered_uploads_detected',
 			__( 'ALLOW_UNFILTERED_UPLOADS is not permitted.', 'wporg-plugins' )
 		);
-
 	}
 
 }
