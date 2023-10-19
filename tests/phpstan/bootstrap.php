101e41dd2cfe0e47e96783b72a851f58ecb411c6
Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>
2023-10-19 17:09:44 +0530

    Apply suggestions from code review
    Co-authored-by: Pascal Birchler <pascalb@google.com>

--- tests/phpstan/bootstrap.php
+++ tests/phpstan/bootstrap.php
@@ -1,6 +1,5 @@
 <?php
 
-defined( 'WP_PLUGIN_CHECK_PLUGIN_DIR_PATH' ) or define( 'WP_PLUGIN_CHECK_PLUGIN_DIR_PATH', '' );
-defined( 'WP_CONTENT_DIR' ) or define( 'WP_CONTENT_DIR', '' );
-defined( 'WP_PLUGIN_DIR' ) or define( 'WP_PLUGIN_DIR', '' );
-defined( 'WP_PLUGIN_CHECK_PLUGIN_DIR_URL' ) or define( 'WP_PLUGIN_CHECK_PLUGIN_DIR_URL', '' );
+define( 'WP_PLUGIN_CHECK_PLUGIN_DIR_PATH', '' );
+define( 'WP_CONTENT_DIR', '' );
+define( 'WP_PLUGIN_CHECK_PLUGIN_DIR_URL', '' );
