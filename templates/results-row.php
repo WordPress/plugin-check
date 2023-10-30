b8fe450689bf553046912338c86d6d7bdbe0ab44
Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>
2023-10-30 10:14:56 +0530

    Apply suggestions from code review
    Co-authored-by: Felix Arntz <felixarntz@users.noreply.github.com>

--- templates/results-row.php
+++ templates/results-row.php
@@ -16,7 +16,7 @@
 	</td>
 	<# if ( data.hasLinks ) { #>
 		<td>
-			<a href="{{data.link}}"<?php echo ( ! has_filter( 'wp_plugin_check_validation_error_source_file_editor_url_template' ) ) ? ' target="_blank"' : '' ?>>
+			<a href="{{data.link}}" target="_blank">
 				<?php esc_html_e( 'View in code editor', 'plugin-check' ); ?>
 				<span class="screen-reader-text"><?php esc_html_e( '(opens in a new tab)', 'plugin-check' ); ?></span>
 			</a>
