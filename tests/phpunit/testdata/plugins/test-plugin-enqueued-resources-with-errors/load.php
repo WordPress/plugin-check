edafb8280555877874ab934008174f8b3ec7fdf9
Nilambar Sharma <nilambar@outlook.com>
2023-12-08 11:48:36 +0545

    Add Enqueued Resources check

---
+++ tests/phpunit/testdata/plugins/test-plugin-enqueued-resources-with-errors/load.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Test Plugin Enqueued Resources check with errors
+ * Plugin URI: https://github.com/wordpress/plugin-check
+ * Description: Test plugin for the Enqueued Resources check.
+ * Requires at least: 6.0
+ * Requires PHP: 5.6
+ * Version: 1.0.0
+ * Author: WordPress Performance Team
+ * Author URI: https://make.wordpress.org/performance/
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: test-plugin-enqueued-resources-with-errors
+ *
+ * @package test-plugin-enqueued-resources-with-errors
+ */
+
+?>
+<link rel="stylesheet" href="http://someurl/somefile.css">
+<script src="http://someurl/somefile.js"></script>
