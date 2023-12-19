72d4df4e73ef9fa32c0622bae2cc6db1d6fd65f6
Nilambar Sharma <nilambar@outlook.com>
2023-12-19 12:22:41 +0545

    Implement readme parser

--- tests/phpunit/tests/Checker/Checks/Plugin_Readme_Check_Tests.php
+++ tests/phpunit/tests/Checker/Checks/Plugin_Readme_Check_Tests.php
@@ -197,4 +197,24 @@ class Plugin_Readme_Check_Tests extends WP_UnitTestCase {
 		$this->assertSame( 0, $check_result->get_error_count() );
 		$this->assertSame( 0, $check_result->get_warning_count() );
 	}
+
+	public function test_run_with_errors_parser_warnings() {
+		$readme_check  = new Plugin_Readme_Check();
+		$check_context = new Check_Context( UNIT_TESTS_PLUGIN_DIR . 'test-plugin-plugin-readme-parser-warnings/load.php' );
+		$check_result  = new Check_Result( $check_context );
+
+		$readme_check->run( $check_result );
+
+		$warnings = $check_result->get_warnings();
+
+		$this->assertNotEmpty( $warnings );
+		$this->assertArrayHasKey( 'readme.txt', $warnings );
+		$this->assertEquals( 1, $check_result->get_warning_count() );
+
+		// Check for parser warning.
+		$this->assertArrayHasKey( 0, $warnings['readme.txt'] );
+		$this->assertArrayHasKey( 0, $warnings['readme.txt'][0] );
+		$this->assertArrayHasKey( 'code', $warnings['readme.txt'][0][0][0] );
+		$this->assertEquals( 'readme_parser_warnings', $warnings['readme.txt'][0][0][0]['code'] );
+	}
 }
