0ca08241ac57f9f5f960b3414da24c1bd6820543
Nilambar Sharma <nilambar@outlook.com>
2024-01-24 11:30:27 +0545

    Add separate error message for empty and invalid plugin name

--- includes/Checker/Checks/Plugin_Readme_Check.php
+++ includes/Checker/Checks/Plugin_Readme_Check.php
@@ -100,15 +100,23 @@ class Plugin_Readme_Check extends Abstract_File_Check {
 	 * @param Parser       $parser      The Parser object.
 	 */
 	private function check_name( Check_Result $result, string $readme_file, Parser $parser ) {
-		if ( empty( $parser->name ) || isset( $parser->warnings['invalid_plugin_name_header'] ) ) {
+		if ( isset( $parser->warnings['invalid_plugin_name_header'] ) ) {
 			$message = sprintf(
 				/* translators: 1: 'Plugin Name' section title, 2: 'Plugin Name' */
-				__( 'We cannot find a plugin name in your readme. Plugin names look like: %1$s. Please change %2$s to reflect the actual name of your plugin.', 'plugin-check' ),
+				__( 'Plugin names look like: %1$s. Please change %2$s to reflect the actual name of your plugin.', 'plugin-check' ),
 				"'=== Plugin Name ==='",
 				"'Plugin Name'"
 			);
 
 			$this->add_result_error_for_file( $result, $message, 'invalid_plugin_name', $readme_file );
+		} elseif ( empty( $parser->name ) ) {
+			$message = sprintf(
+				/* translators: %s: Example plugin name header */
+				__( 'We cannot find a plugin name in your readme. Please update your readme with a valid plugin name header. Eg: %s', 'plugin-check' ),
+				"'=== Example Name ==='"
+			);
+
+			$this->add_result_error_for_file( $result, $message, 'empty_plugin_name', $readme_file );
 		}
 	}
 
