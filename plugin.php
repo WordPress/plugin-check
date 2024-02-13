e3961b46dcc9b588cad85a30bc7f52af1aaeb725
Pascal Birchler <pascalb@google.com>
2024-02-13 18:42:59 +0100

    Update author too

--- plugin.php
+++ plugin.php
@@ -6,7 +6,7 @@
  * Requires at least: 6.3
  * Requires PHP: 7.0
  * Version: 1.0.0
- * Author: WordPress Performance Team
+ * Author: WordPress Performance Team and Plugin Review Team
  * Author URI: https://make.wordpress.org/performance/
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
