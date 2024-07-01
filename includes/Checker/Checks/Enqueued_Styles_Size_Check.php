a0f1385ce035ba22bf540b3f713697c82473981c
Nilambar Sharma <ernilambar@users.noreply.github.com>
2024-07-01 17:49:00 +0545

    Make message translatable
    Co-authored-by: Pascal Birchler <pascal.birchler@gmail.com>

--- includes/Checker/Checks/Enqueued_Styles_Size_Check.php
+++ includes/Checker/Checks/Enqueued_Styles_Size_Check.php
@@ -236,7 +236,8 @@ class Enqueued_Styles_Size_Check extends Abstract_Runtime_Check implements With_
 				$this->add_result_warning_for_file(
 					$result,
 					sprintf(
-						'This style has a size of %1$s which in combination with the other styles enqueued on %2$s exceeds the style size threshold of %3$s.',
+						/* translators: 1: style file size. 2: tested URL. 3: threshold file size. */
+						__( 'This style has a size of %1$s which in combination with the other styles enqueued on %2$s exceeds the style size threshold of %3$s.', 'plugin-check' )
 						size_format( $plugin_style['size'] ),
 						$url,
 						size_format( $this->threshold_size )
