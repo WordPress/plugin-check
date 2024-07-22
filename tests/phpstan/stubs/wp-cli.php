051ce15ba7adea2690f0d92146d65e4c3a22cf3f
Pascal Birchler <pascalb@google.com>
2024-07-22 12:17:21 +0200

    Update stubs

--- tests/phpstan/stubs/wp-cli.php
+++ tests/phpstan/stubs/wp-cli.php
@@ -16,4 +16,22 @@ namespace {
 namespace WP_CLI\Utils {
 	function normalize_path( $text ) {
 	}
+
+	/**
+	 * Return the flag value or, if it's not set, the $default value.
+	 *
+	 * Because flags can be negated (e.g. --no-quiet to negate --quiet), this
+	 * function provides a safer alternative to using
+	 * `isset( $assoc_args['quiet'] )` or similar.
+	 *
+	 * @access public
+	 * @category Input
+	 *
+	 * @param array  $assoc_args Arguments array.
+	 * @param string $flag       Flag to get the value.
+	 * @param mixed  $default    Default value for the flag. Default: NULL.
+	 * @return mixed
+	 */
+	function get_flag_value($assoc_args, $flag, $default = null) {
+	}
 }
