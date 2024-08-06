af11d69c4a55ee1e45a4d1eaffc060483a8f342b
davidperezgar <david@closemarketing.es>
2024-08-06 12:56:54 +0200

    added br

--- includes/Checker/Checks/Plugin_Repo/Plugin_Readme_Check.php
+++ includes/Checker/Checks/Plugin_Repo/Plugin_Readme_Check.php
@@ -181,7 +181,7 @@ class Plugin_Readme_Check extends Abstract_File_Check {
 							$result,
 							sprintf(
 								/* translators: %s: plugin header tag */
-								__( '<strong>Your readme is either missing or incomplete.</strong>The "%s" field is missing. Your readme has to have headers as well as a proper description and documentation as to how it works and how one can use it.', 'plugin-check' ),
+								__( '<strong>Your readme is either missing or incomplete.</strong><br>The "%s" field is missing. Your readme has to have headers as well as a proper description and documentation as to how it works and how one can use it.', 'plugin-check' ),
 								$field['label']
 							),
 							'missing_readme_header',
@@ -217,7 +217,7 @@ class Plugin_Readme_Check extends Abstract_File_Check {
 		) {
 			$this->add_result_warning_for_file(
 				$result,
-				__( '<strong>The readme appears to contain default text.</strong> This means your readme has to have headers as well as a proper description and documentation as to how it works and how one can use it.', 'plugin-check' ),
+				__( '<strong>The readme appears to contain default text.</strong><br> This means your readme has to have headers as well as a proper description and documentation as to how it works and how one can use it.', 'plugin-check' ),
 				'default_readme_text',
 				$readme_file,
 				0,
@@ -245,7 +245,7 @@ class Plugin_Readme_Check extends Abstract_File_Check {
 		if ( empty( $license ) ) {
 			$this->add_result_error_for_file(
 				$result,
-				__( '<strong>Your plugin has no license declared.</strong> Please update your readme with a GPLv2 (or later) compatible license. It is necessary to declare the license of this plugin. You can do this using the fields available both in the plugin readme and in the plugin headers.', 'plugin-check' ),
+				__( '<strong>Your plugin has no license declared.</strong><br> Please update your readme with a GPLv2 (or later) compatible license. It is necessary to declare the license of this plugin. You can do this using the fields available both in the plugin readme and in the plugin headers.', 'plugin-check' ),
 				'no_license',
 				$readme_file,
 				0,
@@ -262,7 +262,7 @@ class Plugin_Readme_Check extends Abstract_File_Check {
 		if ( ! preg_match( '/^([a-z0-9\-\+\.]+)(\sor\s([a-z0-9\-\+\.]+))*$/i', $license ) ) {
 			$this->add_result_warning_for_file(
 				$result,
-				__( '<strong>Your plugin has an invalid license declared.</strong> Please update your readme with a valid SPDX license identifier.', 'plugin-check' ),
+				__( '<strong>Your plugin has an invalid license declared.</strong><br> Please update your readme with a valid SPDX license identifier.', 'plugin-check' ),
 				'invalid_license',
 				$readme_file,
 				0,
@@ -276,7 +276,7 @@ class Plugin_Readme_Check extends Abstract_File_Check {
 		if ( ! $has_license ) {
 			$this->add_result_error_for_file(
 				$result,
-				__( '<strong>Your plugin has no license declared in Plugin Header.</strong> Please update your plugin header with a GPLv2 (or later) compatible license. It is necessary to declare the license of this plugin. You can do this using the fields available both in the plugin readme and in the plugin headers.', 'plugin-check' ),
+				__( '<strong>Your plugin has no license declared in Plugin Header.</strong><br> Please update your plugin header with a GPLv2 (or later) compatible license. It is necessary to declare the license of this plugin. You can do this using the fields available both in the plugin readme and in the plugin headers.', 'plugin-check' ),
 				'no_license',
 				$plugin_main_file,
 				0,
@@ -291,7 +291,7 @@ class Plugin_Readme_Check extends Abstract_File_Check {
 		if ( ! empty( $plugin_license ) && ! preg_match( '/GPL|GNU|MIT|FreeBSD|New BSD|BSD-3-Clause|BSD 3 Clause|OpenLDAP|Expat/im', $plugin_license ) ) {
 			$this->add_result_error_for_file(
 				$result,
-				__( '<strong>Your plugin has an invalid license declared in Plugin Header.</strong> Please update your readme with a valid GPL license identifier. It is necessary to declare the license of this plugin. You can do this using the fields available both in the plugin readme and in the plugin headers.', 'plugin-check' ),
+				__( '<strong>Your plugin has an invalid license declared in Plugin Header.</strong><br> Please update your readme with a valid GPL license identifier. It is necessary to declare the license of this plugin. You can do this using the fields available both in the plugin readme and in the plugin headers.', 'plugin-check' ),
 				'invalid_license',
 				$plugin_main_file,
 				0,
@@ -304,7 +304,7 @@ class Plugin_Readme_Check extends Abstract_File_Check {
 		if ( ! empty( $plugin_license ) && ! empty( $license ) && $license !== $plugin_license ) {
 			$this->add_result_warning_for_file(
 				$result,
-				__( '<strong>Your plugin has a different license declared in the readme file and plugin header.</strong> Please update your readme with a valid GPL license identifier.', 'plugin-check' ),
+				__( '<strong>Your plugin has a different license declared in the readme file and plugin header.</strong><br> Please update your readme with a valid GPL license identifier.', 'plugin-check' ),
 				'license_mismatch',
 				$readme_file,
 				0,
@@ -372,7 +372,7 @@ class Plugin_Readme_Check extends Abstract_File_Check {
 		if ( empty( $stable_tag ) ) {
 			$this->add_result_error_for_file(
 				$result,
-				__( '<strong>Incorrect Stable Tag</strong>Your Stable Tag is meant to be the stable version of your plugin, not of WordPress. For your plugin to be properly downloaded from WordPress.org, those values need to be the same. If they’re out of sync, your users won’t get the right version of your code.', 'plugin-check' ),
+				__( '<strong>Incorrect Stable Tag</strong><br>Your Stable Tag is meant to be the stable version of your plugin, not of WordPress. For your plugin to be properly downloaded from WordPress.org, those values need to be the same. If they’re out of sync, your users won’t get the right version of your code.', 'plugin-check' ),
 				'no_stable_tag',
 				$readme_file,
 				0,
@@ -386,7 +386,7 @@ class Plugin_Readme_Check extends Abstract_File_Check {
 		if ( 'trunk' === $stable_tag ) {
 			$this->add_result_error_for_file(
 				$result,
-				__( "<strong>Incorrect Stable Tag</strong>It's recommended not to use 'Stable Tag: trunk'. Your Stable Tag is meant to be the stable version of your plugin, not of WordPress. For your plugin to be properly downloaded from WordPress.org, those values need to be the same. If they’re out of sync, your users won’t get the right version of your code.", 'plugin-check' ),
+				__( "<strong>Incorrect Stable Tag</strong><br>It's recommended not to use 'Stable Tag: trunk'. Your Stable Tag is meant to be the stable version of your plugin, not of WordPress. For your plugin to be properly downloaded from WordPress.org, those values need to be the same. If they’re out of sync, your users won’t get the right version of your code.", 'plugin-check' ),
 				'trunk_stable_tag',
 				$readme_file,
 				0,
@@ -404,7 +404,7 @@ class Plugin_Readme_Check extends Abstract_File_Check {
 		) {
 			$this->add_result_error_for_file(
 				$result,
-				__( '<strong>The Stable Tag in your readme file does not match the version in your main plugin file.</strong>Your Stable Tag is meant to be the stable version of your plugin, not of WordPress. For your plugin to be properly downloaded from WordPress.org, those values need to be the same. If they’re out of sync, your users won’t get the right version of your code.', 'plugin-check' ),
+				__( '<strong>The Stable Tag in your readme file does not match the version in your main plugin file.</strong><br>Your Stable Tag is meant to be the stable version of your plugin, not of WordPress. For your plugin to be properly downloaded from WordPress.org, those values need to be the same. If they’re out of sync, your users won’t get the right version of your code.', 'plugin-check' ),
 				'stable_tag_mismatch',
 				$readme_file,
 				0,
