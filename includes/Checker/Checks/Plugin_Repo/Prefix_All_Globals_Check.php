da770952f5a8ae7af8b3f523faf1114508371547
davidperezgar <david@closemarketing.es>
2024-08-24 11:58:57 +0200

    moved to WPCS again

--- includes/Checker/Checks/Plugin_Repo/Prefix_All_Globals_Check.php
+++ includes/Checker/Checks/Plugin_Repo/Prefix_All_Globals_Check.php
@@ -8,9 +8,7 @@
 namespace WordPress\Plugin_Check\Checker\Checks\Plugin_Repo;
 
 use WordPress\Plugin_Check\Checker\Check_Categories;
-use WordPress\Plugin_Check\Checker\Check_Result;
-use WordPress\Plugin_Check\Checker\Checks\Abstract_File_Check;
-use WordPress\Plugin_Check\Traits\Amend_Check_Result;
+use WordPress\Plugin_Check\Checker\Checks\Abstract_PHP_CodeSniffer_Check;
 use WordPress\Plugin_Check\Traits\Stable_Check;
 
 /**
@@ -18,9 +16,8 @@ use WordPress\Plugin_Check\Traits\Stable_Check;
  *
  * @since 1.0.2
  */
-class Prefix_All_Globals_Check extends Abstract_File_Check {
+class Prefix_All_Globals_Check extends Abstract_PHP_CodeSniffer_Check {
 
-	use Amend_Check_Result;
 	use Stable_Check;
 
 	/**
@@ -37,61 +34,43 @@ class Prefix_All_Globals_Check extends Abstract_File_Check {
 	}
 
 	/**
-	 * Amends the given result by running the check on the given list of files.
+	 * Returns an associative array of arguments to pass to PHPCS.
 	 *
-	 * @since 1.0.0
+	 * @since 1.0.2
 	 *
-	 * @param Check_Result $result The check result to amend, including the plugin context to check.
-	 * @param array        $files  List of absolute file paths.
-	 *
-	 * @throws Exception Thrown when the check fails with a critical error (unrelated to any errors detected as part of
-	 *                   the check).
+	 * @return array An associative array of PHPCS CLI arguments.
 	 */
-	protected function check_files( Check_Result $result, array $files ) {
-		$php_files = self::filter_files_by_extension( $files, 'php' );
-
-		// Looks for saved plugin options.
-		$this->look_for_name_variables( $result, $php_files );
+	protected function get_args() {
+		return array(
+			'extensions' => 'php',
+			'standard'   => 'WordPress',
+			'sniffs'     => 'WordPress.NamingConventions.PrefixAllGlobals',
+		);
 	}
 
 	/**
-	 * Looks for plugin updater routines in plugin files and amends the given result with an error if found.
+	 * Gets the description for the check.
 	 *
-	 * @since 1.0.0
+	 * Every check must have a short description explaining what the check does.
 	 *
-	 * @param Check_Result $result    The check result to amend, including the plugin context to check.
-	 * @param array        $php_files List of absolute PHP file paths.
+	 * @since 1.1.0
+	 *
+	 * @return string Description.
 	 */
-	protected function look_for_name_variables( Check_Result $result, array $php_files ) {
-
-		foreach ( $php_files as $php_file ) {
-			$lines = file( $php_file );
-			foreach ( $lines as $line_number => $line ) {
-				$matches = array();
-
-				if ( preg_match( '/\$[a-zA-Z0-9_]*\s*=\s*(get_option|add_option|add_site_option|update_option|update_site_option)\(/', $line, $matches ) ) {
-
-
-				}
-			}
-		}
-
-		foreach ( $look_for_regex as $regex ) {
-			$matches      = array();
-			$updater_file = self::file_preg_match( $regex, $php_files, $matches );
-			if ( $updater_file ) {
-				$this->add_result_error_for_file(
-					$result,
-					sprintf(
-						/* translators: %s: The match file name. */
-						__( 'Detected code which may be altering WordPress update routines. Detected: %s', 'plugin-check' ),
-						esc_html( $matches[0] )
-					),
-					'update_modification_detected',
-					$updater_file
-				);
-			}
-		}
+	public function get_description(): string {
+		return __( 'Checks the use of prefixes in all globals.', 'plugin-check' );
 	}
 
+	/**
+	 * Gets the documentation URL for the check.
+	 *
+	 * Every check must have a URL with further information about the check.
+	 *
+	 * @since 1.1.0
+	 *
+	 * @return string The documentation URL.
+	 */
+	public function get_documentation_url(): string {
+		return 'https://developer.wordpress.org/plugins/wordpress-org/common-issues/#prefixing';
+	}
 }
