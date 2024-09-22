fa7c62ad1b1648e752ab1f8688e62d9025ec98f5
Nilambar Sharma <nilambar@outlook.com>
2024-09-22 17:51:08 +0545

    Add unit tests for development functions

--- tests/phpunit/tests/Checker/Checks/Plugin_Review_PHPCS_Check_Tests.php
+++ tests/phpunit/tests/Checker/Checks/Plugin_Review_PHPCS_Check_Tests.php
@@ -54,6 +54,12 @@ class Plugin_Review_PHPCS_Check_Tests extends WP_UnitTestCase {
 
 		// Check for Squiz.PHP.DiscouragedFunctions.Discouraged warning on Line no 20 and column no at 1.
 		$this->assertSame( 'Squiz.PHP.DiscouragedFunctions.Discouraged', $warnings['load.php'][20][1][0]['code'] );
+
+		// Check for WordPress.PHP.DevelopmentFunctions.error_log_var_dump warning on Line no 22 and column no at 1.
+		$this->assertSame( 'WordPress.PHP.DevelopmentFunctions.error_log_var_dump', $warnings['load.php'][22][1][0]['code'] );
+
+		// Check for WordPress.PHP.DevelopmentFunctions.error_log_error_log warning on Line no 23 and column no at 1.
+		$this->assertSame( 'WordPress.PHP.DevelopmentFunctions.error_log_error_log', $warnings['load.php'][23][1][0]['code'] );
 	}
 
 	public function test_run_without_errors() {
