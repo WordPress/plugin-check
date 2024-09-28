87add512a6cfb959b45819afade113a09e0331ed
shail-mehta <shailmehta25@gmail.com>
2024-09-28 19:44:38 +0530

    removed additional space

--- includes/Checker/Runtime_Environment_Setup.php
+++ includes/Checker/Runtime_Environment_Setup.php
@@ -96,7 +96,7 @@ final class Runtime_Environment_Setup {
 	 * @since 1.0.0
 	 *
 	 * @global wpdb $wpdb WordPress database abstraction object.
-	 * @global string $table_prefix  The database table prefix.
+	 * @global string $table_prefix The database table prefix.
 	 * @global WP_Filesystem_Base $wp_filesystem WordPress filesystem subclass.
 	 */
 	public function clean_up() {
