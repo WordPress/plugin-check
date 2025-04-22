7a6a200e1395afdacb1a8ed603d17e011d97cd37
Nilambar Sharma <nilambar@outlook.com>
2025-04-22 10:55:16 +0545

    Update license check tests

--- tests/phpunit/tests/Checker/Checks/Plugin_Readme_Check_Tests.php
+++ tests/phpunit/tests/Checker/Checks/Plugin_Readme_Check_Tests.php
@@ -202,18 +202,6 @@ class Plugin_Readme_Check_Tests extends WP_UnitTestCase {
 		$this->assertCount( 1, wp_list_filter( $errors['readme.txt'][0][0], array( 'code' => 'no_license' ) ) );
 	}
 
-	public function test_run_without_error_mpl2_license() {
-		$readme_check  = new Plugin_Readme_Check();
-		$check_context = new Check_Context( UNIT_TESTS_PLUGIN_DIR . 'test-plugin-plugin-readme-mpl2-license-without-errors/load.php' );
-		$check_result  = new Check_Result( $check_context );
-
-		$readme_check->run( $check_result );
-
-		$errors = $check_result->get_errors();
-
-		$this->assertEmpty( $errors );
-	}
-
 	public function test_run_with_errors_tested_upto() {
 		$readme_check  = new Plugin_Readme_Check();
 		$check_context = new Check_Context( UNIT_TESTS_PLUGIN_DIR . 'test-plugin-plugin-readme-errors-tested-upto/load.php' );
