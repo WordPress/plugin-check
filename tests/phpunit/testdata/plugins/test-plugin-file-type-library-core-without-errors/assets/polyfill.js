97b2271dcf4504ec0b4e90534115e8c7fd9081a1
David Stone <david@nnucomputerwhiz.com>
2026-06-14 13:04:22 -0600

    Improve core library file detection

---
+++ tests/phpunit/testdata/plugins/test-plugin-file-type-library-core-without-errors/assets/polyfill.js
@@ -0,0 +1,5 @@
+/**
+ * Custom feature detection helper that happens to use a generic polyfill.js
+ * filename without bundling WordPress core's registered polyfill script.
+ */
+window.examplePluginPolyfill = true;
