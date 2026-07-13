5c244b210d7cdac53c6c9a43712b936e5e58cd9d
Ishita Jagtap <143935560+ishitaj34@users.noreply.github.com>
2026-07-13 12:30:39 +0530

    fix: resolve sorting

--- tests/phpunit/tests/Checker/Checks/AI_Name_Check_Tests.php
+++ tests/phpunit/tests/Checker/Checks/AI_Name_Check_Tests.php
@@ -1,11 +1,10 @@
- <?php
-
+<?php
 namespace phpunit\tests\Checker\Checks;
 
-use WordPress\Plugin_Check\Checker\Checks\Plugin_Repo\AI_Name_Check;
 use WordPress\Plugin_Check\Checker\AJAX_Runner;
-use WordPress\Plugin_Check\Checker\Check_Result;
 use WordPress\Plugin_Check\Checker\Check_Context;
+use WordPress\Plugin_Check\Checker\Check_Result;
+use WordPress\Plugin_Check\Checker\Checks\Plugin_Repo\AI_Name_Check;
 use WordPress\Plugin_Check\Utilities\Plugin_Request_Utility;
 
 class AI_Name_Check_Tests extends \WP_UnitTestCase {
@@ -20,7 +19,7 @@ class AI_Name_Check_Tests extends \WP_UnitTestCase {
 		$property->setValue( null, $runner );
 
 		$check   = new AI_Name_Check();
-		$context = $this->createMock( Check_Context::class );
+		$context = new Check_Context( WP_PLUGIN_CHECK_MAIN_FILE );
 		$result  = new Check_Result( $context );
 
 		$check->run( $result );
