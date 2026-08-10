6c55e4eff7408dcc0c35cf2c016a330824870971
Nilambar Sharma <nilambar@outlook.com>
2026-08-10 11:19:48 +0545

    Add unit tests for SVN Section and Fetcher classes

---
+++ tests/phpunit/tests/SVN/Fetcher_Tests.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Tests for the Fetcher class.
+ *
+ * @package plugin-check
+ */
+
+namespace SVN;
+
+use WordPress\Plugin_Check\SVN\Fetcher;
+use WP_UnitTestCase;
+
+class Fetcher_Tests extends WP_UnitTestCase {
+
+	protected $fetcher;
+
+	public function set_up() {
+		parent::set_up();
+		$this->fetcher = new Fetcher( 'https://plugins.svn.wordpress.org/example/' );
+	}
+
+	public function test_parse_readme_extracts_fields() {
+		$content = <<<README
+=== My Awesome Plugin ===
+Contributors: someone
+Tags: foo, bar
+Requires at least: 5.0
+Tested up to: 6.4
+Requires PHP: 7.4
+Stable tag: 1.2.3
+
+Description here.
+README;
+
+		$this->assertSame(
+			array(
+				'stable_tag'        => '1.2.3',
+				'requires_at_least' => '5.0',
+				'tested_up_to'      => '6.4',
+				'requires_php'      => '7.4',
+				'name'              => 'My Awesome Plugin',
+			),
+			$this->fetcher->parse_readme( $content )
+		);
+	}
+
+	public function test_parse_readme_supports_markdown_name_header() {
+		$content = <<<README
+# My Awesome Plugin
+
+Stable tag: 2.0.0
+README;
+
+		$data = $this->fetcher->parse_readme( $content );
+
+		$this->assertSame( 'My Awesome Plugin', $data['name'] );
+		$this->assertSame( '2.0.0', $data['stable_tag'] );
+	}
+
+	public function test_parse_readme_returns_empty_array_for_no_matches() {
+		$this->assertSame( array(), $this->fetcher->parse_readme( 'Just some plain text.' ) );
+	}
+
+	public function test_parse_plugin_headers_extracts_fields() {
+		$content = <<<PHP
+<?php
+/**
+ * Plugin Name: My Awesome Plugin
+ * Version: 1.2.3
+ * Requires at least: 5.0
+ * Tested up to: 6.4
+ * Requires PHP: 7.4
+ * Author: John Doe
+ */
+PHP;
+
+		$this->assertSame(
+			array(
+				'Plugin Name'       => 'My Awesome Plugin',
+				'Version'           => '1.2.3',
+				'Requires at least' => '5.0',
+				'Tested up to'      => '6.4',
+				'Requires PHP'      => '7.4',
+				'Author'            => 'John Doe',
+			),
+			$this->fetcher->parse_plugin_headers( $content )
+		);
+	}
+
+	public function test_parse_plugin_headers_returns_empty_array_for_no_matches() {
+		$content = <<<PHP
+<?php
+echo 'Hello world';
+PHP;
+
+		$this->assertSame( array(), $this->fetcher->parse_plugin_headers( $content ) );
+	}
+}
